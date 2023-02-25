438f724988611d5faaf22552b1c8993257ed6859
Anastasiia Redkina <renatell424@gmail.com>
2023-02-26 00:11:46 +0400

    add Random in ex.005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What is you name?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Hello, pupun'ka");
+}
+else 
+{
+Console.Write("Hello, ");
+Console.Write(username);
+}
